b120c2adfccb8fb54413bb9c1e390f3e27d33a41
GabrielL <79658132+gluna1203@users.noreply.github.com>
2021-11-23 10:44:28 -0700

    Cookie Parser

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const { MongoClient, ObjectId } = require('mongodb')
 const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser')
+
 
 const url = 'mongodb+srv://Joey:SirPentious@cluster0.dps0f.mongodb.net/myData?retryWrites=true&w=majority'; // Using ATLAS
 const client = new MongoClient(url);
