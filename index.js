d11bab9ef6da4d02075f3bf2dc0e78bdcc78b8e0
GabrielL <79658132+gluna1203@users.noreply.github.com>
2021-11-29 10:11:34 -0700

    Small Changes

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 
 app.use(cookieParser('whatever'))
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + "/views");
